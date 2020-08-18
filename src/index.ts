050054e826f79b07a90b6f1fa0023edabb58ba48
Pavel Lichmaniuk <Pavel Lichmaniuk>
2020-08-18 14:46:39 +0300

    add user controller

--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,12 @@
 import express from 'express';
-import { v4 as uuid } from 'uuid';
+import { UserController } from './user/user.controller';
 
 const app = express();
-const router = express.Router();
 const port = 8080;
-
-type User = {
-  id: string;
-  login: string;
-  password: string;
-  age: number;
-  isDeleted: boolean;
-};
-const users: User[] = [];
-
-function getAutoSuggestUsers(loginSubstring: string, limit: number): User[] {
-  const userItems = users
-    .filter((user) => user.login.includes(loginSubstring))
-    .sort((a, b) => a.login > b.login ? -1 : 1)
-    .slice(limit - 1);
-
-  return userItems;
-}
+const userController = new UserController();
 
 app.use(express.json());
-
-// Get autoSuggestion
-router.get('/users', (req, res) => {
-  const userItems = getAutoSuggestUsers('et', 5);
-
-  if (!userItems) {
-    res.status(404);
-    return res.send('There is no user with such id or it was deleted');
-  }
-
-  res.send(userItems);
-});
-
-// Get by id
-router.get('/user/:id', (req, res) => {
-  const userItems = users.find(
-    (user) => user.id === req.params.id && !user.isDeleted
-  );
-
-  if (!userItems) {
-    res.status(404);
-    return res.send('There is no user with such id or it was deleted');
-  }
-
-  res.send(userItems);
-});
-
-// Create
-router.post('/user', (req, res) => {
-  const id = uuid();
-  users.push({
-    login: req.body.login,
-    password: req.body.password,
-    age: req.body.age,
-    isDeleted: false,
-    id,
-  });
-  res.send(id);
-});
-
-// Update
-router.post('/user/:id', (req, res) => {
-  const userIndex = users.findIndex(
-    (user) => user.id === req.params.id && !user.isDeleted
-  );
-
-  if (userIndex === -1) {
-    res.status(404);
-    return res.send('There is no user with such id or it was deleted');
-  }
-
-  users[userIndex] = {
-    ...users[userIndex],
-    login: req.body.login,
-    password: req.body.password,
-    age: req.body.age,
-  };
-  res.send(users[userIndex]);
-});
-
-// Delete
-router.delete('/user/:id', (req, res) => {
-  const userIndex = users.findIndex((user) => user.id === req.params.id);
-
-  if (userIndex === -1) {
-    res.status(404);
-    return res.send('No user with such id');
-  }
-
-  users[userIndex].isDeleted = true;
-  res.sendStatus(200);
-});
-
-app.use('/', router);
+app.use('/', userController.router);
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
---
+++ src/user/user.controller.ts
@@ -0,0 +1,99 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+import { User } from './user.interface';
+
+export class UserController {
+  public router = Router();
+  public path = '/users';
+  private users: User[];
+
+  constructor() {
+    this.users = [];
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    this.router.get(`${this.path}`, this.getUsers.bind(this));
+    this.router.get(`${this.path}/:id`, this.getUserById.bind(this));
+    this.router.post(`${this.path}`, this.createUser.bind(this));
+    this.router.post(`${this.path}/:id`, this.updateUser.bind(this));
+    this.router.delete(`${this.path}/:id`, this.deleteUser.bind(this));
+  }
+
+  private getUsers(req: any, res: any) {
+    const userItems = this.getAutoSuggestUsers('et', 5);
+
+    if (!userItems) {
+      res.status(404);
+      return res.send('There is no user with such id or it was deleted');
+    }
+
+    res.send(userItems);
+  }
+
+  private getUserById(req: any, res: any) {
+    const userItems = this.users.find(
+      (user) => user.id === req.params.id && !user.isDeleted
+    );
+
+    if (!userItems) {
+      res.status(404);
+      return res.send('There is no user with such id or it was deleted');
+    }
+
+    res.send(userItems);
+  }
+
+  private createUser(req: any, res: any) {
+    const id = uuid();
+    this.users.push({
+      login: req.body.login,
+      password: req.body.password,
+      age: req.body.age,
+      isDeleted: false,
+      id,
+    });
+    res.send(id);
+  }
+
+  private updateUser(req: any, res: any) {
+    const userIndex = this.users.findIndex(
+      (user) => user.id === req.params.id && !user.isDeleted
+    );
+
+    if (userIndex === -1) {
+      res.status(404);
+      return res.send('There is no user with such id or it was deleted');
+    }
+
+    this.users[userIndex] = {
+      ...this.users[userIndex],
+      login: req.body.login,
+      password: req.body.password,
+      age: req.body.age,
+    };
+    res.send(this.users[userIndex]);
+  }
+
+  private deleteUser(req: any, res: any) {
+    const userIndex = this.users.findIndex((user) => user.id === req.params.id);
+
+    if (userIndex === -1) {
+      res.status(404);
+      return res.send('No user with such id');
+    }
+
+    this.users[userIndex].isDeleted = true;
+    res.sendStatus(200);
+  }
+
+  private getAutoSuggestUsers(loginSubstring: string, limit: number): User[] {
+    const userItems = this.users;
+    // const userItems = this.users
+    //   .filter((user) => user.login.includes(loginSubstring))
+    //   .sort((a, b) => (a.login > b.login ? -1 : 1))
+    //   .slice(limit - 1);
+
+    return userItems;
+  }
+}
